e2068c9cf543229c7aadd62252b31983f21e9e6c
RohanKumarJha <rohan.jha0107@gmail.com>
2026-08-30 00:45:29 +0530

    Conditional rendering

--- 7. React Projects/0. Healthy food list/healthy-food-list/src/App.jsx
+++ 7. React Projects/0. Healthy food list/healthy-food-list/src/App.jsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
 
-  let foodItems = ['Dal', 'Green Vegetable', 'Roti', 'Salad', 'Milk', 'Ghee'];
+  let foodItems = [];
+  // let foodItems = ['Dal', 'Green Vegetable', 'Roti', 'Salad', 'Milk', 'Ghee'];
 
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 ? <h3>I'm still hungry.</h3> : null}
       <ul className="list-group">
         {foodItems.map(item =>
           <li key={item} className="list-group-item">{item}</li>
