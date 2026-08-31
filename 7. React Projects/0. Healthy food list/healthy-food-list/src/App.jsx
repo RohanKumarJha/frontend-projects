7637dafce02ecf71a9236405e919abcd7f0bca30
RohanKumarJha <rohan.jha0107@gmail.com>
2026-08-31 09:25:57 +0530

    Passing children

--- 7. React Projects/0. Healthy food list/healthy-food-list/src/App.jsx
+++ 7. React Projects/0. Healthy food list/healthy-food-list/src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import FoodItems from './components/FoodItems';
 import ErrorMessage from './components/ErrorMessage';
 import './App.css';
+import Container from './components/Container';
 
 const App = () => {
 
@@ -9,11 +10,11 @@ const App = () => {
   let foodItems = ['Dal', 'Green Vegetable', 'Roti', 'Salad', 'Milk', 'Ghee'];
 
   return (
-    <>
+    <Container>
       <h1 className='food-heading'>Healthy Food</h1>
       <ErrorMessage foodItems={foodItems} />
       <FoodItems foodItems={foodItems} />
-    </>
+    </Container>
   );
 }
 
--- 7. React Projects/0. Healthy food list/healthy-food-list/src/components/Item.jsx
+++ 7. React Projects/0. Healthy food list/healthy-food-list/src/components/Item.jsx
@@ -5,6 +5,9 @@ const Item = ({ item }) => {
   return (
     <li className={`${styles.MyItem} list-group-item`}>
       <span className={`${styles.MySpan}`}>{item}</span>
+      <button className={`${styles.button} btn btn-info`}>
+        Buy
+      </button>
     </li>
   );
 }
