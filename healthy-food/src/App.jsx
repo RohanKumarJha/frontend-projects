158428071677bc10009d81beccd5238b4292a0e3
RohanKumarJha <rohan.jha0107@gmail.com>
2026-09-08 00:15:14 +0530

    Project-1 (Healthy-food) : React fragement

---
+++ healthy-food/src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return <>
+    <h1>Healthy food</h1>
+    <ul className="list-group">
+      <li className="list-group-item">Dal</li>
+      <li className="list-group-item">Green vegetable</li>
+      <li className="list-group-item">Roti</li>
+      <li className="list-group-item">Salad</li>
+      <li className="list-group-item">Milk</li>
+    </ul>
+  </>
+}
+
+export default App;
