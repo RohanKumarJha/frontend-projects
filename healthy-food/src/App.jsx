2653ab69909ed7e0a8f233e4d31038b4c8817e9f
RohanKumarJha <rohan.jha0107@gmail.com>
2026-09-08 01:25:43 +0530

    Healthy food : Conditional Rendering

--- healthy-food/src/App.jsx
+++ healthy-food/src/App.jsx
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-  let foodItems = ["Dal", "Green vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  let foodItems = [];
+  // let foodItems = ["Dal", "Green vegetable", "Roti", "Salad", "Milk", "Ghee"];
 
   return <>
     <h1>Healthy food</h1>
+    {foodItems.length === 0 && <h3>I'm still hungry</h3>};
     <ul className="list-group">
       {foodItems.map(item => (
         <li key={item} className="list-group-item">{item}</li>
